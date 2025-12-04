20ca41b72d84de25635c12102da2a7a7a93f8a03
uuummm <urata88648810@gmail.com>
2025-12-04 17:33:51 +0900

    SignalControlの変更

--- include/SignalControl.h
+++ include/SignalControl.h
@@ -1,4 +1,5 @@
 #ifndef SignalControl
 #define SignalControl
 int signalCharJudgment(char signal_char);
+int SignalMolsJudgment(int count);
 #endif
--- src/SignalControl.c
+++ src/SignalControl.c
@@ -1,8 +1,28 @@
 #include "CommonObjectControler.h"
+#include "ExcuteVBScript.h"
 
 int signalCharJudgment(char signal_char){
 
     switch (signal_char){
+    case '0':
+        excuteVBScript("./bin.RunMacro1.vbs");
+
+        return 0;
+    case '1':
+        comControlerVBA("sample.vba");
+
+        return 0;
+    
+    default:
+        break;
+    }
+
+    //signal_charをもとにコードが実行されないのなら1を戻す
+    return 1;
+}
+
+int signalMolsJudgment(int signalCount){
+        switch (signalCount){
     case '0':
         comControlerVBA("sample.vba");
 

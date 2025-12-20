62f26cdbf573dc073c58983b5f4da9607c036e97
uuummm8810 <urata88648810@gmail.com>
2025-12-20 23:30:08 +0900

    VBAマクロ実行の関数化

--- src/ExcuteVBscript.c
+++ src/ExcuteVBscript.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
 #include <windows.h>
 
-int excuteVBScript(char filename[200]){
+int excuteVBScript(char macroName[200]){
     char currentPath[MAX_PATH];
     GetModuleFileName(NULL, currentPath, MAX_PATH);
     
     char cmd[1024];//サイズを十分の大きい値に設定
     char arg1[480];
 
-    //変数の代入でエラーのため下記は一時停止
-    //↓
-    //arg1=vbsPath;
+    char projectDirectory,macroBookPath;
+    char vbScriptPath;
+    sprintf(vbScriptPath, "%s/src/ExcuteVBA.vbs",projectDirectory);
+    sprintf(macroBookPath,"%s/bin/MacroBook.xlsm", projectDirectory);
     
-    // \"%s\" とすることで、実行時には "パス" として展開される
-    //sprintf(cmd, "cscript //nologo %s/src/\"%s\" \"%s\"", currentPath,filename,currentPath);
+    sprintf(cmd, "cscript //nologo \"%s\" \"%s\" \"%s\"", vbScriptPath, macroBookPath, macroName);
     //引き渡し先VBScriptの引数 は第一引数がMacroWorkBookPathである
     
-    //testコード
-    //環境次第で動作しないので必ず変更すること
-    //
-    
-    system("cscript //nologo D:/COM_PortEventListener/src/RunMacro1.vbs D:\\COM_PortEventListener\\bin\\MacroBook.xlsm");
+    system(cmd);
     return 0;
 }

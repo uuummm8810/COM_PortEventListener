3e038f681f4086b96eacef1daa9de9e72ea39d36
uuummm8810 <urata88648810@gmail.com>
2025-12-20 20:26:40 +0900

    信号受信間隔を用いた通信コードの削除

--- src/SignalControl.c
+++ src/SignalControl.c
@@ -35,17 +35,7 @@ int signalCharJudgment(char signal_char){
     return 1;
 }
 
-int signalMolsJudgment(int signalCount){
-        switch (signalCount){
-    case '0':
-        comControlerVBA("sample.vba");
-
-        return 0;
-    
-    default:
-        break;
     }
 
     //signal_charをもとにコードが実行されないのなら1を戻す
     return 1;
-}

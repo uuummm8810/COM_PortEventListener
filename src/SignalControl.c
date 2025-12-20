501effb97fc59ee6e463539a9325ffbea56736c0
uuummm8810 <urata88648810@gmail.com>
2025-12-20 20:27:31 +0900

    Excelマクロを呼び出すVBScriptの統合

--- src/SignalControl.c
+++ src/SignalControl.c
@@ -7,34 +7,28 @@
 int signalCharJudgment(char signal_char){
 
     switch (signal_char){
-    case '0':
-        system("cscript //nologo D:/COM_PortEventListener/src/RunMacro1.vbs D:\\COM_PortEventListener\\bin\\MacroBook.xlsm");
-        return 0;
-    case '1':
-         system("cscript //nologo D:/COM_PortEventListener/src/RunAverage.vbs D:\\COM_PortEventListener\\bin\\MacroBook.xlsm");
-        return 0;
-    
-    case '2':
-        system("cscript //nologo D:/COM_PortEventListener/src/RunLinEstAboveCell.vbs D:\\COM_PortEventListener\\bin\\MacroBook.xlsm");
-    
-        return 0;
-
-    case '3':
-        system("cscript //nologo D:/COM_PortEventListener/src/RunCalcStdevPAboveCell.vbs D:\\COM_PortEventListener\\bin\\MacroBook.xlsm");
-        return 0;
-
-    case 'd':
-        system("cscript //nologo D:/COM_PortEventListener/src/ExcuteVBA.vbs D:\\COM_PortEventListener\\bin\\MacroBook.xlsm Macro1");
-        return 0;
-    default:
-    
-        break;
-    }
-
-    //signal_charをもとにコードが実行されないのなら1を戻す
-    return 1;
-}
-
+        case '0':
+            system("cscript //nologo D:/COM_PortEventListener/src/ExcuteVBA.vbs D:\\COM_PortEventListener\\bin\\MacroBook.xlsm SumBelowBlank");
+            return 0;
+            
+        case '1':
+            system("cscript //nologo D:/COM_PortEventListener/src/ExcuteVBA.vbs D:\\COM_PortEventListener\\bin\\MacroBook.xlsm AverageWithBlankCheck");
+            return 0;
+        
+        case '2':
+            system("cscript //nologo D:/COM_PortEventListener/src/ExcuteVBA.vbs D:\\COM_PortEventListener\\bin\\MacroBook.xlsm LinEstAboveCell");
+            return 0;
+        
+        case '3':
+            system("cscript //nologo D:/COM_PortEventListener/src/ExcuteVBA.vbs D:\\COM_PortEventListener\\bin\\MacroBook.xlsm CalcStdevPAboveCell");
+            return 0;
+        
+        case 'd':
+            system("cscript //nologo D:/COM_PortEventListener/src/ExcuteVBA.vbs D:\\COM_PortEventListener\\bin\\MacroBook.xlsm Macro1");
+            return 0;
+        
+        default:
+            break;
     }
 
     //signal_charをもとにコードが実行されないのなら1を戻す

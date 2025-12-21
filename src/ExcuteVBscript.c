27c60b9d9354eeb0126bd2692008efe2a32172b7
uuummm8810 <urata88648810@gmail.com>
2025-12-21 14:00:40 +0900

    exeのcurentPathを取得してからprojectのディレクトリを取得する処理の実装

--- src/ExcuteVBscript.c
+++ src/ExcuteVBscript.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <windows.h>
+#include <shlwapi.h>
 
 int excuteVBScript(char macroName[200]){
-    char currentPath[MAX_PATH];
-    GetModuleFileName(NULL, currentPath, MAX_PATH);
-    
     char cmd[1024];//サイズを十分の大きい値に設定
-    char arg1[480];
+    char currentPath[MAX_PATH], projectDirectoryPath[MAX_PATH], macroBookPath[MAX_PATH], vbScriptPath[MAX_PATH];    
 
-    char projectDirectory,macroBookPath;
-    char vbScriptPath;
-    sprintf(vbScriptPath, "%s/src/ExcuteVBA.vbs",projectDirectory);
-    sprintf(macroBookPath,"%s/bin/MacroBook.xlsm", projectDirectory);
+    GetModuleFileName(NULL,currentPath,MAX_PATH);//第一引数HMODULEがNULLなら現在のプロセスのPATHを取得し第二引数にコピー
+    strcpy(projectDirectoryPath,currentPath);
+    PathRemoveFileSpec(projectDirectoryPath);//コピー元のcurentPathから実行ファイル名を削る
+    PathRemoveFileSpec(projectDirectoryPath);//コピー元のcurentPathから実行ファイルの親ディレクトリ(bin)を削る
+
+    //下記のsprintfはprojectDirectoryPathが長すぎると正常に動作しない
+    //projectファイルをあまりディレクトリの入れ子とならないようにすること
+    sprintf(vbScriptPath, "%240s/src/ExcuteVBA.vbs",projectDirectoryPath);
+    sprintf(macroBookPath,"%240s/bin/MacroBook.xlsm", projectDirectoryPath);
     
-    sprintf(cmd, "cscript //nologo \"%s\" \"%s\" \"%s\"", vbScriptPath, macroBookPath, macroName);
     //引き渡し先VBScriptの引数 は第一引数がMacroWorkBookPathである
-    
+    sprintf(cmd, "cscript //nologo \"%s\" \"%s\" \"%s\"", vbScriptPath, macroBookPath, macroName);
     system(cmd);
+    
     return 0;
 }

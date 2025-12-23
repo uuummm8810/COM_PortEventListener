de64911744c0789d454bb523a91a17f2d9614baa
uuummm <urata88648810@gmail.com>
2025-12-23 15:13:50 +0900

    test関数の位置の変更
    暗黙的な宣言の修正

--- src/Main.c
+++ src/Main.c
@@ -1,8 +1,15 @@
+#include <windows.h>
 #include <stdio.h>
 #include "EventListenerDebug.h"
 #include "ExcuteVBScript.h"
 
 //test
+int test(){
+    //system("cscript //nologo C:\\Users\\uuumm\\Documents\\GitHub\\COM_PortEventListener\\src\\ExcuteVBA.vbs C:\\Users\\uuumm\\Documents\\GitHub\\COM_PortEventListener\\bin\\MacroBook.xlsm Macro1");
+    excuteVBScript("Macro1");
+    return 0;
+}
+
 int main(){
     test();
     //selectPort();
@@ -10,7 +17,3 @@ int main(){
     return 0;
 }
 
-int test(){
-    excuteVBScript("Macro1");
-    return 0;
-}

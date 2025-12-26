7997408f70a0f90d0b27bd9b87345d5a568b9fef
uuummm8810 <urata88648810@gmail.com>
2025-12-26 21:17:58 +0900

    test関数の修正

--- src/Main.c
+++ src/Main.c
@@ -5,7 +5,6 @@
 
 //test
 int test(){
-    //system("cscript //nologo C:\\Users\\uuumm\\Documents\\GitHub\\COM_PortEventListener\\src\\ExcuteVBA.vbs C:\\Users\\uuumm\\Documents\\GitHub\\COM_PortEventListener\\bin\\MacroBook.xlsm Macro1");
     excuteVBScript("Macro1");
     return 0;
 }

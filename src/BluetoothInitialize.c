6400ad21699703829fba0e2de401dfb6bc482f8d
uuummm8810 <urata88648810@gmail.com>
2026-01-15 01:03:20 +0900

    Bluetoothデバイスの認識機能追加

--- src/BluetoothInitialize.c
+++ src/BluetoothInitialize.c
@@ -1,7 +1,10 @@
 #include <windows.h>
 #include <bluetoothapis.h>
+#include <string.h>
 
 int initializeBluetooth() {
+
+    const wchar_t* bluetoothDeviceName = L"Experiment_Module_01";
     
     HANDLE hRadio = NULL;
     BLUETOOTH_FIND_RADIO_PARAMS btFindRadioParams = { sizeof(BLUETOOTH_FIND_RADIO_PARAMS) };
@@ -16,5 +19,41 @@ int initializeBluetooth() {
     BluetoothFindRadioClose(hFind);
     CloseHandle(hRadio);
 
+    BLUETOOTH_DEVICE_INFO deviceInfo = { sizeof(BLUETOOTH_DEVICE_INFO) };
+    if (findBluetoothDevice(bluetoothDeviceName, &deviceInfo) == 0) {
+        printf("Device not found.\n");
+        return 1;
+    }
+
+    return 0;
+}
+
+int findBluetoothDevice(const wchar_t* targetName, BLUETOOTH_DEVICE_INFO* outInfo) {
+    BLUETOOTH_DEVICE_SEARCH_PARAMS searchParams = {
+        sizeof(BLUETOOTH_DEVICE_SEARCH_PARAMS),
+        TRUE,
+        TRUE,
+        TRUE,
+        TRUE,
+        TRUE,
+        10, // *1.28 seconds timeout
+        NULL
+    };
+
+    BLUETOOTH_DEVICE_INFO deviceInfo = { sizeof(BLUETOOTH_DEVICE_INFO) };
+    HBLUETOOTH_DEVICE_FIND hFind = BluetoothFindFirstDevice(&searchParams, &deviceInfo);
+
+    if (hFind == NULL) return 1;
+
+    do {
+        // 名前を比較 (ワイド文字列なので wcscmp を使用)
+        if (wcscmp(deviceInfo.szName, targetName) == 0) {
+            *outInfo = deviceInfo; // 見つかったら情報をコピー
+            BluetoothFindDeviceClose(hFind);
+            return TRUE;
+        }
+    } while (BluetoothFindNextDevice(hFind, &deviceInfo));
+
+    BluetoothFindDeviceClose(hFind);
     return 0;
 }
